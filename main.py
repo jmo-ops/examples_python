fd85c2d11a4c98e4634132d2acf5fc5651059df8
J Mo <>
2021-04-08 02:32:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# print commatnd prints text to screen
+print("hello lego")
+# print a new line
+print("\n")
+#The answers to these
+#calculations will be printed when
+#the program is run.
+print(50 + 50)
+
+#name is a variable –
+#something that can
+#change.
+name = input("What is your name ? ")
+print('hello ' + name)
